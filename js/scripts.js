3096db32479fa15aaae60d08e1af03c0d6e3b5b5
Beverly Potts <pottsbeverly@gmail.com>
2020-06-12 14:08:02 -0400

    Put input into a string array, then use .map to create an array of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
-$("form#form1").submit(function(event){
-  event.preventDefault();
-  const userNumber = parseInt($("#input").val());
-  
-   if(!Number.isInteger(userNumber)){
-    $("#output").text("Please enter a numeral");
-   }else {
-    $("#output").text("This is a number");
-   } 
-});
+  $("form#form1").submit(function(event){
+    event.preventDefault();
+    const inputString = $("#input").val();
+    const stringArray = inputString.split("");
+    const numberArray = stringArray.map(function(element) {
+      return parseInt(element);
+    });
+    
+    $("#output").text(numberArray);
+    
+   // if(!Number.isInteger(userNumber)){
+     // $("#output").text("Please enter a numeral");
+   // }else {
+      //$("#output").text("This is a number");
+   // } 
+  });
 });
 
