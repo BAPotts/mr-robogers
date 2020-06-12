7f862bded3c70ecadf852bdf7072892f954a1dee
Beverly Potts <pottsbeverly@gmail.com>
2020-06-12 12:18:48 -0400

    Add function to determine whether input is a number or not

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,16 @@ $("form#form1").submit(function(event){
   event.preventDefault();
   const userNumber = parseInt($("#input").val());
   
+  const output = integerIdentifier(userNumber);
 
   $("#output").text(output);
   });
 });
+
+function integerIdentifier(userNumber){
+  if(Number.isInteger(userNumber)){
+    return "true";
+  } else {
+    return "false";
+  }
+}
